8dbea55cff3e9b7948b166a2f04f5eeb76227810
Chrome1209 <malisalhafidz12@gmail.com>
2023-10-26 16:13:34 +0700

    pesan github

---
+++ README.md
@@ -0,0 +1,3 @@
+# APLIKASI posUjian
+
+MEYUPLAI BARANG
---
+++ home.php
@@ -0,0 +1,12 @@
+<div>
+    <center><h2> Ujian Kelas TKJ </h2></center>
+    <img src="images/tjk.jpg" alt="tkj" width="30%" class="rounded mx-auto d-block">
+    <p>Penyuplai Oksigen Terendam Air, Petambang Emas Ilegal Banyumas Belum Ditemukan</p>
+    <ul>
+        <li>PURWOKERTO, KOMPAS — Memasuki hari kelima, pencarian delapan petambang emas ilegal di Desa Pancurendang, Kecamatan Ajibarang, Banyumas, Jawa Tengah, belum membuahkan hasil. Padahal, alat penyuplai oksigen di lubang tambang sudah rusak sejak beberapa hari lalu.</li>
+        <li>Sejauh ini, berbagai cara evakuasi sudah dilakukan. Selain menyiapkan 25 pompa penyedot air, tim pencari juga membendung Sungai Tajur dan Sungai Datar. Tujuannya, mencegah air menggenangi lubang.</li>
+        <li>”Penyedotan sudah membuat ketinggian air turun sekitar 4 meter,” kata Kepala Subseksi Operasi dan Siaga di Badan SAR Nasional (Basarnas) Cilacap Priyo Prayudha Utama, Minggu (30/7/2023) siang.</li>
+        <li>Akan tetapi, Priyo mengatakan belum bisa menggunakan ekskavator. Tim khawatir, pengoperasian alat itu bakal memicu bencana lain di sekitar lokasi kejadian.</li>
+    </ul>
+    <p>”Jika dipaksakan, takutnya malah likuefikasi,” ujar Priyo.</p>
+</div>
